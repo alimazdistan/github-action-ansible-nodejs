476593898c119fd9fc1933b4abe70180cd600537
Ali Mazdistan <167213351+alimazdistan@users.noreply.github.com>
2025-05-20 16:06:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
   res.send('Hello from EC2');
 });
 
-const PORT = 3000;
+const PORT = 80;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
